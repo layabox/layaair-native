0031ae13f64f491975ef21bb0a8c3a3a3b068891
lvfulong <775331175@qq.com>
2018-10-09 14:38:49 +0800

    单机版 https

--- appCommand.ts
+++ appCommand.ts
@@ -6,8 +6,8 @@ import child_process = require('child_process');
 import  xmldom = require('xmldom');
 import * as ProgressBar from 'progress';
 
-export const STAND_ALONE_URL: string = 'http://stand.alone.version/runtime.json';
-export const VERSION_CONFIG_URL: string = 'http://www.layabox.com/layanative2.0/layanativeRes/versionconfig.json';
+export const STAND_ALONE_URL: string = 'http://stand.alone.version/index.js';
+export const VERSION_CONFIG_URL: string = 'https://www.layabox.com/layanative2.0/layanativeRes/versionconfig.json';
 export const DEFAULT_NAME: string = 'LayaBox';
 export const DEFAULT_APP_NAME: string = 'LayaBox';
 export const DEFAULT_PACKAGE_NAME: string = 'com.layabox.game';
